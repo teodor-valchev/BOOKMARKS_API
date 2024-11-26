234fd15fa9f220309fd66a8fe0e7d89460ec7e23
Teodor Valchev <teo.valxev@gmail.com>
2024-11-26 19:32:30 +0200

    created users info endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
-    
+    @Get('me')
+    getMe() {
+        return 'user info'
+    }
 }
