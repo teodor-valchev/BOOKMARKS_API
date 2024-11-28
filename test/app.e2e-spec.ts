1fcecdd884e71dc0f107a372fd23817ebd80d5eb
Teodor Valchev <teo.valxev@gmail.com>
2024-11-28 10:38:04 +0200

    Installed dotenv cli
    -> created scripts for testing db
    -> created copy of dev-db

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,32 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import {
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 
 describe('App e2e', () => {
+  let app: INestApplication;
   beforeAll(async () => {
     const moduleRef =
       await Test.createTestingModule({
         imports: [AppModule],
       }).compile();
+
+    const app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // whatever is in the dto defined that will be used!!!
+      }),
+    );
+
+    await app.init();
+  });
+
+  afterAll(() => {
+    if (app) {
+      app.close();
+    }
   });
   it.todo('Should pass');
-  it.todo('Should pass 2');
 });
